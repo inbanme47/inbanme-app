e44c6fea30c5c1426859ad5983593d1c7482518c
Your Name <you@example.com>
2026-09-24 15:56:47 +0700

    Fix duplicate path declaration

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,15 @@ const multer = require('multer');
 const mongoose = require('mongoose');
 const app = express();
 const PORT = 5000;
-const path = require('path');
+
 // Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // Tự động tạo thư mục 'uploads' nếu chưa có
 const uploadDir = path.join(__dirname, 'uploads');
