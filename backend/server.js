0e56947deb1ee03759cdeeaefc3ce7b7170596c6
Your Name <you@example.com>
2026-09-24 16:03:43 +0700

    Fix static path for index.html

--- backend/server.js
+++ backend/server.js
@@ -10,11 +10,7 @@ const PORT = 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
 
 // Tự động tạo thư mục 'uploads' nếu chưa có
 const uploadDir = path.join(__dirname, 'uploads');
